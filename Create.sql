c80371271fbc40e61b48a8e77a1b6b2cb2788f9e
Sameer Ulhaq <sameer.ulhaq79@gmail.com>
2023-09-19 13:21:23 +0500

    SCHEMA ADDED

---
+++ Create.sql
@@ -0,0 +1,32 @@
+USE [inventory]
+GO
+
+CREATE TABLE [dbo].[product](
+	[id] [int] identity(1,1) NOT NULL,
+	[name] [varchar](255) NULL,
+	[price] [decimal](18, 2) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[sale]    Script Date: 19/09/2023 1:16:27 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[sale](
+	[id] [int] identity(1,1) NOT NULL,
+	[product_id] [int] NULL,
+	[qty] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[sale]  WITH CHECK ADD FOREIGN KEY([product_id])
+REFERENCES [dbo].[product] ([id])
+GO
+
